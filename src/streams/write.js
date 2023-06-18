c6a431c2cb5e2a1c643558fb40c6723cd8aa2614
Rodion <101873493+HuffPuffTuff@users.noreply.github.com>
2023-06-18 18:18:57 +0400

    feat: complete streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import { createWriteStream } from 'fs';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { Writable } from 'stream';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const filePath = resolve(__dirname, 'files', 'fileToWrite.txt');
+  const writableStream = createWriteStream(filePath, 'utf-8');
+
+  const { stdin, stdout } = process;
+
+  const exitCommand = ['exit'];
+
+  stdin.on('data', (data) => {
+    const buffer = data.toString().trim();
+
+    if (exitCommand.includes(buffer)) {
+      process.exit();
+    } else {
+      writableStream.write(data);
+      stdout.write('Hi, write your text: ');
+    }
+  });
+
+  process.on('exit', () => stdout.write('By, by'));
+
+  process.on('SIGINT', () => {
+    stdout.write('By, by');
+  });
 };
 
 await write();
