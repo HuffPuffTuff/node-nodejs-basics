a997962d3f44ee69345a5d1826c25bdb50a91989
Rodion <101873493+HuffPuffTuff@users.noreply.github.com>
2023-06-19 09:08:14 +0400

    feat: complete zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { resolve } from 'path';
+import { createWriteStream, createReadStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const fileToCompressPath = resolve(__dirname, 'files', 'fileToCompress.txt');
+  const compressedFilePath = resolve(__dirname, 'files', 'archive.gz');
+
+  const readStream = createReadStream(fileToCompressPath);
+  const writeStream = createWriteStream(compressedFilePath);
+
+  const gzip = createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.log(
+      `File fileToCompress.txt compressed to archive.gz successfully.`
+    );
+  });
+
+  writeStream.on('error', (err) => {
+    console.error('An error occurred during compression:', err);
+  });
 };
 
 await compress();
